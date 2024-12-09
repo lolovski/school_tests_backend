378da5401db2ac7078c6d89717575600b7212ec1
lolovski <alexpaxyaev@gmail.com>
2024-12-09 13:24:14 +0300

    card

--- app/api/routers/card.py
+++ app/api/routers/card.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
-from typing import List
+from typing import List, Union
 
 from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi.params import Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.routers.utils import check_object_exists, check_name_duplicate
@@ -88,4 +89,92 @@ async def delete_card(
     return card
 
 
+@router.get(
+    "/card_task/",
+    response_model=Union[List[CardTaskRead], CardTaskRead],
+    tags=['card task']
+)
+async def get_card_tasks(
+        task_id: Optional[int] = Query(None, description="Task ID"),
+        card_id: Optional[int] = Query(None, description="Card ID"),
+        session: AsyncSession = Depends(get_async_session),
+):
+    if card_id is not None and task_id is not None:
+        cards = await card_task_requests.get(session=session, task_id=task_id, card_id=card_id)
+        return cards
+    elif task_id is not None:
+        cards = await card_task_requests.get(session=session, task_id=task_id)
+    elif card_id is not None:
+        cards = await card_task_requests.get(session=session, card_id=card_id)
+    else:
+        cards = await card_task_requests.get_multi(session=session)
 
+    return cards
+
+
+@router.post(
+    '/card_task/',
+    response_model=CardTaskRead,
+    tags=['card task']
+)
+async def create_card_task(
+        card_task: CardTaskCreate,
+        session: AsyncSession = Depends(get_async_session),
+):
+    await check_card_task_duplicate(card_id=card_task.card_id,task_id=card_task.task_id, session=session)
+    db_card_task = await card_task_requests.create(obj_in=card_task, session=session)
+    return db_card_task
+
+
+@router.patch(
+    '/card_task/',
+    response_model=CardTaskRead,
+    tags=['card task']
+)
+async def update_card(
+        update_in: CardTaskUpdate,
+        task_id: int = Query(..., description="Task ID"),
+        card_id: int = Query(..., description="Card ID"),
+        session: AsyncSession = Depends(get_async_session),
+):
+    card_task = await check_card_task_exists(task_id=task_id, card_id=card_id, session=session)
+    await check_card_task_duplicate(card_id=card_task.card_id, task_id=card_task.task_id, session=session)
+    card_task = await card_task_requests.update(card_task, update_in, session=session)
+    return card_task
+
+
+@router.delete(
+    '/card_task/',
+    response_model=CardTaskRead,
+    response_model_exclude_none=True,
+    tags=['card task']
+)
+async def delete_card(
+        task_id: int = Query(..., description="Task ID"),
+        card_id: int = Query(..., description="Card ID"),
+        session: AsyncSession = Depends(get_async_session),
+):
+    card_task = await check_card_task_exists(task_id=task_id, card_id=card_id, session=session)
+    card_task = await card_task_requests.remove(card_task, session=session)
+    return card_task
+
+
+async def check_card_task_exists(
+        task_id: int,
+        card_id: int,
+        session: AsyncSession,
+):
+    card_task = await card_task_requests.get(session=session, task_id=task_id, card_id=card_id)
+    if card_task is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Card task not found")
+    return card_task
+
+
+async def check_card_task_duplicate(
+        task_id: int,
+        card_id: int,
+        session: AsyncSession,
+):
+    card_task = await card_task_requests.get(session=session, task_id=task_id, card_id=card_id)
+    if card_task is not None:
+        raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Card task already exists")
--- requests/card.py
+++ requests/card.py
@@ -13,6 +13,41 @@ class CardRequests(RequestsBase):
 card_requests = CardRequests(Card)
 
 class CardTaskRequests(RequestsBase):
-    ...
+    async def get(
+            self,
+            session: AsyncSession,
+            task_id: int = None,
+            card_id: int = None,
+    ):
+        if task_id is None and card_id is None:
+
+            card_task = await session.scalar(
+                select(CardTask)
+                .where(
+                    CardTask.task_id == task_id,
+                    CardTask.card_id == card_id,
+                )
+            )
+            return card_task
+        elif task_id is not None:
+            card_task = await session.scalars(
+                select(CardTask)
+                .where(
+                    CardTask.task_id == task_id,
+                )
+            )
+        elif card_id is not None:
+            card_task = await session.scalars(
+                select(CardTask)
+                .where(
+                    CardTask.card_id == card_id,
+                )
+            )
+        else:
+            card_task = await session.scalars(
+                select(CardTask)
+            )
+        return card_task.all()
+
 
 card_task_requests = CardTaskRequests(CardTask)
