cb2ee8e25cbfd7d90c6668dad4cb8e612f18be9b
lolovski <alexpaxyaev@gmail.com>
2025-02-26 16:03:39 +0300

    start

--- core/auth.py
+++ core/auth.py
@@ -2,8 +2,12 @@ from fastapi_users.authentication import AuthenticationBackend, CookieTransport,
 
 from core.config import settings
 
-cookie_transport = CookieTransport()
-cookie_admin_transport = CookieTransport()
+cookie_transport = CookieTransport(
+    cookie_name="access_token",
+    cookie_max_age=3600,
+    cookie_secure=True,  # Установите в True, если используете HTTPS
+    cookie_samesite="None",
+)
 
 
 def get_jwt_strategy() -> JWTStrategy:
